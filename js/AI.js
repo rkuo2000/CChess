77f71162f4a60ac831a9bec83ecc5339909a2173
Richard Kuo <rkuo2000@gmail.com>
2024-10-01 13:11:28 +0800

    Update AI.js

--- js/AI.js
+++ js/AI.js
@@ -2,12 +2,12 @@
 
 var AI = AI||{};
 
-AI.historyTable	=	{};		//历史表
+AI.historyTable	=	{};		//歷史表
 
 
 //人工智能初始化
 AI.init = function(pace){
-	var bill = AI.historyBill || com.gambit; //开局库
+	var bill = AI.historyBill || com.gambit; //開局庫
 	if (bill.length){
 		var len=pace.length;
 		var arr=[];
@@ -26,7 +26,7 @@ AI.init = function(pace){
 		}
 		
 	}
-	 //如果棋谱里面没有，人工智能开始运作
+	 //如果棋谱里面没有，人工智能開始運作
 	var initTime = new Date().getTime();
 	AI.treeDepth=play.depth;
 	//AI.treeDepth=4;
@@ -77,7 +77,7 @@ AI.iterativeSearch = function (map, my){
 	return false;
 }
 
-//取得棋盘上所有棋子
+//取得棋盤上所有棋子
 AI.getMapAllMan = function (map, my){
 	var mans=[];
 	for (var i=0; i<map.length; i++){
@@ -107,7 +107,7 @@ AI.getMoves = function (map, my){
 			var y=man.y;
 			var newX=val[n][0];
 			var newY=val[n][1];
-			 //如果不是长将着法
+			 //如果不是長將着法
 			if (foul[0]!=x || foul[1]!=y || foul[2]!=newX || foul[3]!=newY ){
 				moves.push([x,y,newX,newY,man.key])
 			}
@@ -115,7 +115,7 @@ AI.getMoves = function (map, my){
 	}
 	return moves;
 }
-//A:当前棋手value/B:对手value/depth：层级
+//A:當前棋手value/B:對手value/depth：層级
 AI.getAlphaBeta = function (A, B, depth, map ,my) {
 	//var txtMap= map.join();
 	//var history=AI.historyTable[txtMap];
@@ -123,7 +123,7 @@ AI.getAlphaBeta = function (A, B, depth, map ,my) {
 	//		return 	history.value*my;
 	//}
 	if (depth == 0) {
-		return {"value":AI.evaluate(map , my)}; //局面评价函数;
+		return {"value":AI.evaluate(map , my)}; //局面評價函數;
 　	}
 　	var moves = AI.getMoves(map , my ); //生成全部走法;
 　	//这里排序以后会增加效率
@@ -131,7 +131,7 @@ AI.getAlphaBeta = function (A, B, depth, map ,my) {
 	for (var i=0; i < moves.length; i++) {
 	
 	
-　　	//走这个走法;
+　　	//走這個走法;
 		var move= moves[i];
 		var key = move[4];
 		var oldX= move[0];
@@ -145,7 +145,7 @@ AI.getAlphaBeta = function (A, B, depth, map ,my) {
 		play.mans[key].x = newX;
 		play.mans[key].y = newY;
 		
-	　　if (clearKey=="j0"||clearKey=="J0") {//被吃老将,撤消这个走法;
+	　　if (clearKey=="j0"||clearKey=="J0") {//被吃老將,撤消這個走法;
 			play.mans[key]	.x = oldX;
 			play.mans[key]	.y = oldY;
 			map[ oldY ][ oldX ] = key;
@@ -161,7 +161,7 @@ AI.getAlphaBeta = function (A, B, depth, map ,my) {
 	　　	var val = -AI.getAlphaBeta(-B, -A, depth - 1, map , -my).value;
 			//val = val || val.value;
 	
-	　　	//撤消这个走法;　
+	　　	//撤消這個走法;　
 			play.mans[key]	.x = oldX;
 			play.mans[key]	.y = oldY;
 			map[ oldY ][ oldX ] = key;
@@ -171,7 +171,7 @@ AI.getAlphaBeta = function (A, B, depth, map ,my) {
 				 //play.mans[ clearKey ].isShow = true;
 			}
 	　　	if (val >= B) {
-				//将这个走法记录到历史表中;
+				//將這個走法記錄到歷史表中;
 				//AI.setHistoryTable(txtMap,AI.treeDepth-depth+1,B,my);
 				return {"key":key,"x":newX,"y":newY,"value":B};
 			}
@@ -181,27 +181,27 @@ AI.getAlphaBeta = function (A, B, depth, map ,my) {
 			}
 		}
 　	}
-	//将这个走法记录到历史表中;
+	//將這個走法記錄到歷史表中;
 	//AI.setHistoryTable(txtMap,AI.treeDepth-depth+1,A,my);
-	if (AI.treeDepth == depth) {//已经递归回根了
+	if (AI.treeDepth == depth) {//已经遞歸回根了
 		if (!rootKey){
-			//AI没有最佳走法，说明AI被将死了，返回false
+			//AI没有最佳走法，說明AI被將死了，返回false
 			return false;
 		}else{
-			//这个就是最佳走法;
+			//這個就是最佳走法;
 			return rootKey;
 		}
 	}
 　return {"key":key,"x":newX,"y":newY,"value":A};
 }
 
-//奖着法记录到历史表
+//獎着法記錄到歷史表
 AI.setHistoryTable = function (txtMap,depth,value,my){
 	AI.setHistoryTable.lenght ++;
 	AI.historyTable[txtMap] = {depth:depth,value:value}
 }
 
-//评估棋局 取得棋盘双方棋子价值差
+//評估棋局 取得棋盘双方棋子價值差
 AI.evaluate = function (map,my){
 	var val=0;
 	for (var i=0; i<map.length; i++){
@@ -212,7 +212,7 @@ AI.evaluate = function (map,my){
 			}
 		}
 	}
-	//val+=Math.floor( Math.random() * 10);  //让AI走棋增加随机元素
+	//val+=Math.floor( Math.random() * 10);  //讓AI走棋增加隨機元素
 	//com.show()
 	//z(val*my)
 	AI.number++;
